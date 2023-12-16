325bd1497f3b56f3eac4b5f0d3f7a631ece8f006
Alber9arri <alberto.arribas@estudiantes.uva.es>
2023-12-16 02:43:37 +0100

    API 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.precticafinalstewardle"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.precticafinalstewardle"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
