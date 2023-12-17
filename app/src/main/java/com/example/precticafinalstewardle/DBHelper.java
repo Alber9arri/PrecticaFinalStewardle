6b0f44d2ea3d8b2f868844af1caff6d347e89291
GonzaloAgudo <g.aguvir@gmail.com>
2023-12-17 16:07:10 +0100

    inicio sesion v1.0

---
+++ app/src/main/java/com/example/precticafinalstewardle/DBHelper.java
@@ -0,0 +1,31 @@
+package com.example.precticafinalstewardle;
+
+// DBHelper.java
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "app_db";
+    private static final int DATABASE_VERSION = 1;
+
+    private static final String CREATE_TABLE_USERS = "CREATE TABLE users " +
+            "(id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT)";
+
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_TABLE_USERS);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS users");
+        onCreate(db);
+    }
+}
+
---
+++ app/src/main/java/com/example/precticafinalstewardle/User.java
@@ -0,0 +1,43 @@
+// User.java
+package com.example.precticafinalstewardle;
+
+public class User {
+    private int id;
+    private String username;
+    private String password;
+
+    // Constructor
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    // Getter and Setter methods for id
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    // Getter and Setter methods for username
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    // Getter and Setter methods for password
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
+
+
