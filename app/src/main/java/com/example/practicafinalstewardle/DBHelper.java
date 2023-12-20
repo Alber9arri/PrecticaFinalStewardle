5eeb41a40af66a11ea3cbd4dd67aaf61caee2a38
GonzaloAgudo <g.aguvir@gmail.com>
2023-12-20 08:48:41 +0100

    Diseño pantalla login

--- app/src/main/java/com/example/practicafinalstewardle/DBHelper.java
+++ app/src/main/java/com/example/practicafinalstewardle/DBHelper.java
@@ -11,7 +11,7 @@ public class DBHelper extends SQLiteOpenHelper {
     private static final int DATABASE_VERSION = 1;
 
     private static final String CREATE_TABLE_USERS = "CREATE TABLE users " +
-            "(id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT)";
+            "(id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT, played INTEGER, win INTEGER, lost INTEGER)";
 
     public DBHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
