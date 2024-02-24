44996e2d4a1c049d024b121bc95abfaa58faeb10
Alber9arri <alberto.arribas@estudiantes.uva.es>
2024-02-24 02:07:00 +0100

    Version play store

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.rinko.practicafinalstewardle"
         minSdk = 25
         targetSdk = 34
-        versionCode = 6
-        versionName = "6.0"
+        versionCode = 7
+        versionName = "7.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
